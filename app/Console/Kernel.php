fd9b2ffe8ac42f3063a730e2345df76462f986d6
Rashed Hasan <rashed50702@gmail.com>
2021-12-29 02:13:48 +0600

    sync with live server + scheduling tested

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('midnight:update')->daily();
+        $schedule->command('midnight:update')->everyMinute();
     }
 
     /**
